95b82bc264c25ff09c6a77610b135428d4734c05
Ranithaa28 <ranithaaravichandran@gmail.com>
2026-05-16 22:16:55 +0530

    Connected frontend to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
 import axios from "axios";
 import "./App.css";
 
-const API = const API =
- "https://online-auction-platform-fh47.onrender.com/api/test";
+const API =
+  "https://online-auction-platform-fh47.onrender.com/api/test";
 
 function App() {
 
   const connectBackend = async () => {
+
     try {
+
       const response = await axios.get(API);
-      alert("Backend Connected Successfully");
-      console.log(response.data);
+
+      alert(response.data);
+
     } catch (error) {
-      alert("Backend Connection Failed");
+
       console.log(error);
+
+      alert("Backend Connection Failed");
     }
   };
 
   return (
     <div className="container">
+
       <h1>Auction Bazaar</h1>
 
       <button onClick={connectBackend}>
         Connect Backend
       </button>
+
     </div>
   );
 }
